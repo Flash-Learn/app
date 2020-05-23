82f88615f9e7adab2b2af349934e2406d1b8a620
samay-kothari <2001samay@gmail.com>
2020-05-23 12:25:03 +0530

    added the feature of google sign in and also UI pages of login, register and my decks, also added the class of users

--- microlearning/lib/main.dart
+++ microlearning/lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:microlearning/screens/authentication/login.dart';
+import 'package:microlearning/screens/authentication/register.dart';
+import 'package:microlearning/screens/mydecks.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return LoginUser();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginUser(),
+        '/home': (context) => MyDecks(),
+        '/register': (context) => RegisterUser(),
+      },
+    );
   }
 }
--- microlearning/lib/screens/authentication/login.dart
+++ microlearning/lib/screens/authentication/login.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:microlearning/screens/authentication/register.dart';
 
 class LoginUser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[200],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            FlatButton(
+              child: Text('NewUser? Register'),
+              onPressed: () {
+                return Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) {return RegisterUser();}));
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
