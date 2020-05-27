63705e17d72e6beee57b8618c8194dac4036d748
siddhant jain <siddhant.jain120@gmail.com>
2020-05-27 19:43:37 +0530

    added padding to the flashcards

--- microlearning/lib/helperWidgets/flashCardView.dart
+++ microlearning/lib/helperWidgets/flashCardView.dart
@@ -18,18 +18,21 @@ class FlashCardView extends StatelessWidget {
   Widget build(BuildContext context) {
     double relativePosition = currentIndex - currentPage;
 
-    return Container(
-      color: color,
-      width: 300,
-      child: Transform(
-        transform: Matrix4.identity()
-          ..setEntry(3, 2, 0.003)
-          ..scale(0.5)
-          ..rotateY(relativePosition),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: color,
+        width: 300,
+        child: Transform(
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.003)
+            ..scale(0.5)
+            ..rotateY(relativePosition),
 
-        alignment: relativePosition >= 0
-          ? Alignment.centerLeft
-          : Alignment.centerRight,
+          alignment: relativePosition >= 0
+            ? Alignment.centerLeft
+            : Alignment.centerRight,
+        ),
       ),
     );
   }
