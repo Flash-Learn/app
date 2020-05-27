c8bc8b351af9ebdf683154d7ac70059d31aef35c
siddhant jain <siddhant.jain120@gmail.com>
2020-05-27 19:39:32 +0530

    created swiping animation for flashcards

---
+++ microlearning/lib/helperWidgets/flashCardView.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class FlashCardView extends StatelessWidget {
+
+  final Color color;
+  final int currentIndex;
+  final double currentPage;
+  final String flashCardID;
+
+  FlashCardView({
+    this.color,
+    this.currentIndex,
+    this.currentPage,
+    this.flashCardID,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double relativePosition = currentIndex - currentPage;
+
+    return Container(
+      color: color,
+      width: 300,
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.003)
+          ..scale(0.5)
+          ..rotateY(relativePosition),
+
+        alignment: relativePosition >= 0
+          ? Alignment.centerLeft
+          : Alignment.centerRight,
+      ),
+    );
+  }
+}
--- microlearning/lib/screens/viewDeck.dart
+++ microlearning/lib/screens/viewDeck.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:microlearning/classes/deck.dart';
 import 'package:microlearning/helperFunctions/getDeckFromID.dart';
+import 'package:microlearning/helperWidgets/flashCardView.dart';
 
 class ViewDeck extends StatefulWidget {
   final String deckID;
@@ -23,14 +24,77 @@ class _ViewDeckState extends State<ViewDeck> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
-          deckID,
+          deck.deckName,
         ),
+      ),
+      body: FlashCardSwipeView(
+        deck: deck,
       ),
     );
   }
   Deck _getThingsOnStartup(){
     Deck deck = getDeckFromID(deckID);
     return deck;
+  }
+}
+
+class FlashCardSwipeView extends StatefulWidget {
+  @override
+  FlashCardSwipeView({
+    this.deck,
+  });
+  final Deck deck;
+  _FlashCardSwipeViewState createState() => _FlashCardSwipeViewState(deck: deck);
+}
+
+class _FlashCardSwipeViewState extends State<FlashCardSwipeView> {
+
+  _FlashCardSwipeViewState({
+    this.deck,
+  });
+  final Deck deck;
+  final PageController _pageCtrl = PageController(viewportFraction: 0.8);
+
+  double currentPage = 0.0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageCtrl.addListener(() {
+      setState(() {
+        currentPage = _pageCtrl.page;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+            height: 500,
+            color: Colors.white,
+            child: PageView.builder(
+              controller: _pageCtrl,
+              scrollDirection: Axis.horizontal,
+              itemCount: deck.flashCardList.length,
+              itemBuilder: (context, int currentIndex) {
+                return FlashCardView(
+                  color: Colors.accents[currentIndex],
+                  currentIndex: currentIndex,
+                  currentPage: currentPage,
+                  flashCardID: deck.flashCardList[currentIndex],
+                );
+              }
+            ),
+          ),
+      ],
+    );
   }
 }
