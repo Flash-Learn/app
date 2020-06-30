dca8564495b0f9042e9dfa15453ca3c2144be13c
siddhant jain <siddhant.jain120@gmail.com>
2020-06-30 18:52:14 +0530

    updated UI for Mydecks

--- microlearning/lib/Utilities/Widgets/deckInfoCard.dart
+++ microlearning/lib/Utilities/Widgets/deckInfoCard.dart
@@ -6,6 +6,33 @@ import 'package:microlearning/Utilities/constants/color_scheme.dart';
 
 Widget deckInfoCard(String deckID) {
 
+  List<Widget> getTags(List<dynamic> strings)
+  {
+    List<Widget> ret = [];
+    for(var i = 0; i < strings.length; i++){
+      ret.add(
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              color: (i%2 == 0) ? Color.fromRGBO(242, 201, 76, 1) : Color.fromRGBO(187, 107, 217, 1),
+            ),
+//            color: Color.fromRGBO(242, 201, 76, 1),
+            child: Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Text(
+                strings[i],
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 0, 0, 0.5),
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          )
+      );
+      ret.add(SizedBox(width: 5,));
+    }
+    return ret;
+  }
 
 
   return StreamBuilder(
@@ -25,12 +52,12 @@ Widget deckInfoCard(String deckID) {
       print(snapshot);
       return Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
           child: Container(
-            height: 100,
-            width: 300,
+            height: 110,
+            width: MediaQuery. of(context). size. width,
             child: Material(
-              elevation: 2,
+              elevation: 3,
               borderRadius: BorderRadius.circular(10),
               color: MyColorScheme.uno(),
               shadowColor: Colors.black,
@@ -45,40 +72,45 @@ Widget deckInfoCard(String deckID) {
                         padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
                         child: Row(
                           children: <Widget>[
-                            Icon(
-                              Icons.filter_none,
-                              color: MyColorScheme.accent(),
-                            ),
+//                            Icon(
+//                              Icons.filter_none,
+//                              color: MyColorScheme.accent(),
+//                            ),
                             SizedBox(
-                              width: 2,
+                              width: 10,
                             ),
                             Text(
                               deck["deckName"],
                               style: TextStyle(
                                   color: MyColorScheme.cinco(),
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold),
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w900
+                              ),
                                   overflow: TextOverflow.ellipsis,
                             ),
                           ],
                         )),
+                    SizedBox(height: 5,),
                     SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Row(
                           children: <Widget>[
-                            Icon(
-                              Icons.bookmark,
-                              color: MyColorScheme.accent(),
-                            ),
+//                            Icon(
+//                              Icons.bookmark,
+//                              color: MyColorScheme.accent(),
+//                            ),
                             SizedBox(
-                              width: 2,
+                              width: 10,
                             ),
-                            Text(
-                              deck["tagsList"].join(" "),
-                              style: TextStyle(
-                                color: MyColorScheme.quatro(),
-                              ),
-                            ),
+                            Row(
+                              children: getTags(deck["tagsList"]),
+                            )
+//                            Text(
+//                              deck["tagsList"].join(" "),
+//                              style: TextStyle(
+//                                color: MyColorScheme.quatro(),
+//                              ),
+//                            ),
                           ],
                         ))
                   ],
