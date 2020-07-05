2832d7da63a0c61d87ceb654c2b919f4a31ddba4
samay-kothari <2001samay@gmail.com>
2020-07-05 17:26:05 +0530

    fixes oriention issues, made it potrait only

--- microlearning/lib/main.dart
+++ microlearning/lib/main.dart
@@ -7,20 +7,33 @@ import 'package:microlearning/screens/authentication/register.dart';
 import 'package:microlearning/screens/Decks/my_decks.dart';
 import 'package:microlearning/screens/authentication/welcome.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   var email = prefs.getString('email');
-  runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Montserrat'),
-    routes: {
-      '/': (context) => email == null ? WelcomeScreen() : MyDecks(),
-      '/login': (context) => LoginUser(),
-      '/home': (context) => MyDecks(),
-      '/register': (context) => RegisterUser(),
-      '/search': (context) => Search(),
-      '/notificationinapp': (context) => NotificationInapp(),
-    },
-  ));
+  return runApp(MyApp(email: email,));
+}
+class MyApp extends StatelessWidget {
+  final email;
+  MyApp({@required this.email});
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Montserrat'),
+      routes: {
+        '/': (context) => email == null ? WelcomeScreen() : MyDecks(),
+        '/login': (context) => LoginUser(),
+        '/home': (context) => MyDecks(),
+        '/register': (context) => RegisterUser(),
+        '/search': (context) => Search(),
+        '/notificationinapp': (context) => NotificationInapp(),
+      },
+    );
+  }
 }
