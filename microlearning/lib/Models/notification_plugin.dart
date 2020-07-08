b20e93ed6dec94be09766ede49b46546ac3356ac
Keshav Bajaj <keshavb2712@gmail.com>
2020-07-08 07:57:29 +0530

    notifications working on release too

--- microlearning/lib/Models/notification_plugin.dart
+++ microlearning/lib/Models/notification_plugin.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 var i = -999999;
@@ -18,12 +19,21 @@ class Notifications {
 
   Future singleNotification(DateTime datetime, String message, String subtext,
       {String sound}) async {
+    var vibrationPattern = new Int64List(4);
+    vibrationPattern[0] = 0;
+    vibrationPattern[1] = 1000;
+    vibrationPattern[2] = 5000;
+    vibrationPattern[3] = 2000;
+
     var androidChannel = AndroidNotificationDetails(
       'channel-id',
       'channel-name',
       'channel-description',
       playSound: true,
       importance: Importance.Max,
+      vibrationPattern: vibrationPattern,
+      ongoing: true,
+      enableLights: true,
       priority: Priority.Max,
     );
 
