48f36347dbe936772ca4ee78e5fa56ee42630fad
siddhant jain <siddhant.jain120@gmail.com>
2020-07-09 22:02:07 +0530

    show group page on tapping the group card

--- microlearning/lib/screens/Groups/group.dart
+++ microlearning/lib/screens/Groups/group.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:microlearning/Models/group.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:microlearning/Utilities/constants/loading.dart';
+import 'package:microlearning/screens/Groups/init_group.dart';
 
 class Group extends StatefulWidget {
+
+  final String groupID;
+
+  Group({
+    this.groupID,
+  });
+
   @override
   _GroupState createState() => _GroupState();
 }
@@ -8,6 +19,44 @@ class Group extends StatefulWidget {
 class _GroupState extends State<Group> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Firestore.instance
+          .collection('groups')
+          .document(widget.groupID)
+          .snapshots(),
+      builder: (context, snapshot){
+        if (!snapshot.hasData) {
+          return Loading(size: 50,);
+        }
+
+        GroupData group = GroupData(
+          groupID: widget.groupID,
+          description: snapshot.data["description"],
+          name: snapshot.data["name"],
+          decks: snapshot.data["decks"],
+          users: snapshot.data["users"],
+        );
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              snapshot.data["name"],
+            ),
+            centerTitle: true,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return InitGroup(groupData: group,);
+                  }));
+                },
+              )
+            ],
+          ),
+        );
+      }
+
+    );
   }
 }
