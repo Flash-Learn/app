865d40d55ef37f9819276c9ed1308e3925e0550b
Tejasvi Chebrolu <tejasvicsr1@gmail.com>
2020-07-10 23:18:47 +0530

    Added indices to Algolia.

--- microlearning/functions/index.js
+++ microlearning/functions/index.js
@@ -5,13 +5,14 @@ const APP_ID = functions.config().algolia.app;
 
 const client = algoliasearch(functions.config().algolia.appid, functions.config().algolia.apikey);
 const index  = client.initIndex('decks');
+const user_index = client.initIndex('user_data');
 
 exports.addToIndex = functions.firestore.document('decks/{deckId}')
 
     .onCreate(snapshot => {
         const data = snapshot.data();
         const objectID = snapshot.id;
-        console.log(data);
+        // console.log(data);
         return index.saveObject({...data, objectID});
     });
 
@@ -27,3 +28,30 @@ exports.updateIndex = functions.firestore.document('decks/{deckId}')
 exports.deleteFromIndex = functions.firestore.document('decks/{deckId}')
 
     .onDelete(snapshot => index.deleteObject(snapshot.id));
+
+// Group Search starts from here.
+
+exports.addToUserIndex = functions.firestore.document('user_data/{userId}') //can be an error here.
+
+    .onCreate(snapshot => {
+        const data = snapshot.data();
+        const objectID = snapshot.id;
+        console.log(data);
+        return user_index.saveObject({...data, objectID});
+    });
+
+exports.updateUserIndex = functions.firestore.document('user_data/{userId}')
+
+    .onUpdate((change) => {
+        const newData = change.after.data();
+        const objectID = change.after.id;
+
+        return user_index.saveObject({ ...newData, objectID});
+    });
+
+exports.deleteFromUserIndex = functions.firestore.document('user_data/{userId}')
+
+    .onDelete(snapshot => user_index.deleteObject(snapshot.id));
+
+
+
