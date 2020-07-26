4b8b2fb0c0e305b4df06af013c5dd70d8035d1e0
samay-kothari <2001samay@gmail.com>
2020-07-26 14:48:23 +0530

    adding a copy of deck when adding it to a grp from mydecks

--- microlearning/lib/Utilities/functions/saveDeck.dart
+++ microlearning/lib/Utilities/functions/saveDeck.dart
@@ -30,9 +30,15 @@ void saveDeck(context, Deck deck, String originalDeckID) async {
       tempCard = doc.data;
     });
     final flashRef = await flashcardsReference.add({
+      // 'term': tempCard["term"],
+      // 'definition': tempCard["definition"],
+      // 'isimage': tempCard["isimage"],
       'term': tempCard["term"],
       'definition': tempCard["definition"],
-      'isimage': tempCard["isimage"],
+      'isTermPhoto': tempCard["isTermPhoto"],
+      'isDefinitionPhoto': tempCard["isDefinitionPhoto"],
+      'isOneSided': tempCard["isOneSided"],
+
     });
     print(flashRef.documentID);
     toSave.flashCardList.add(flashRef.documentID);
@@ -66,4 +72,63 @@ void saveDeck(context, Deck deck, String originalDeckID) async {
     MaterialPageRoute(builder: (context) => MyDecks()),
     (Route<dynamic> route) => false,
   );
+}
+
+void saveDecktoGroup(context, Deck deck, String originalDeckID, String grpId) async {
+  print(deck.deckName);
+  Deck toSave = Deck(
+    deckName: deck.deckName,
+    tagsList: deck.tagsList,
+    isPublic: false,
+    downloads: 0,
+  );
+  toSave.isPublic = false;
+  toSave.deckID = null;
+  toSave.flashCardList = [];
+
+
+  final CollectionReference decksReference =
+      Firestore.instance.collection("decks");
+  final CollectionReference flashcardsReference =
+      Firestore.instance.collection("flashcards");
+  final CollectionReference userReference =
+      Firestore.instance.collection("user_data");
+  for (var i = 0; i < deck.flashCardList.length; i++) {
+    dynamic tempCard;
+    await flashcardsReference.document(deck.flashCardList[i]).get().then((doc) {
+      tempCard = doc.data;
+    });
+    final flashRef = await flashcardsReference.add({
+      // 'term': tempCard["term"],
+      // 'definition': tempCard["definition"],
+      // 'isimage': tempCard["isimage"],
+      'term': tempCard["term"],
+      'definition': tempCard["definition"],
+      'isTermPhoto': tempCard["isTermPhoto"],
+      'isDefinitionPhoto': tempCard["isDefinitionPhoto"],
+      'isOneSided': tempCard["isOneSided"],
+
+    });
+    print(flashRef.documentID);
+    toSave.flashCardList.add(flashRef.documentID);
+  }
+
+  final deckRef = await decksReference.add({
+    'deckName': toSave.deckName,
+    'tagsList': toSave.tagsList,
+    'flashcardList': toSave.flashCardList,
+    'isPublic': false,
+    'downloads': 0,
+  });
+
+  print("deck ID:");
+  print(deckRef.documentID);
+
+  await Firestore.instance.collection("groups").document(grpId).updateData({
+    "decks": FieldValue.arrayUnion([deckRef.documentID]),
+  });
+
+  decksReference.document(deckRef.documentID).updateData({
+    'deckID': deckRef.documentID,
+  });
 }
